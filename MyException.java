8ebe7cc0ac93ad0c62470d88aaefb8af92d36ee9
poojaamin450 <69665490+poojaamin450@users.noreply.github.com>
2022-01-16 12:37:21 +0530

    Add files via upload

--- MyException.java
+++ MyException.java
@@ -1,12 +1,8 @@
 
-public class MyException extends Exception {
-	String str1;
-	   MyException(String str2) {
-		str1=str2;
-	   }
-	   public String toString(){ 
-		return ("MyException Occurred: "+str1) ;
-	   }
-	}
+class MyException extends Exception {
+	public MyException(String s) 
+    { 
+        super(s); 
+    } 
 
-
+}
